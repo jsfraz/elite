0727ac302bc4eed10ac713d581855eb1cc38921a
Josef Ráž <josefraz@seznam.cz>
2025-11-10 16:53:14 +0100

    Updated darkman script

--- .local/share/darkman/darkman.sh
+++ .local/share/darkman/darkman.sh
@@ -5,6 +5,14 @@
 export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
 swaymsg reload
 
+# Check whether to send notification
+if [ ! -f ~/.config/sway/config.json ]; then
+    exit 0
+fi
+if [ "$(darkman get)" = "$(jq -r '.mode' ~/.config/sway/config.json 2>/dev/null)" ]; then
+    exit 0
+fi
+
 # Wait for notification service to be available (max 5 seconds)
 if ! pgrep -x mako > /dev/null; then
     for i in {1..10}; do
