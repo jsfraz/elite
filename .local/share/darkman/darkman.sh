7871a62bcee9d7fe3cb92dbaf5ebb24718e16e76
Josef Ráž <josefraz@seznam.cz>
2025-11-10 17:08:51 +0100

    Updated darkman script

--- .local/share/darkman/darkman.sh
+++ .local/share/darkman/darkman.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-# Restart sway config
-# https://github.com/swaywm/sway/issues/3769#issuecomment-1807181019
-export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
-swaymsg reload
+# Wait for Sway to be running and socket to be available (max 10 seconds)
+for i in {1..20}; do
+    SWAY_PID=$(pgrep -x sway)
+    if [ -n "$SWAY_PID" ]; then
+        # https://github.com/swaywm/sway/issues/3769#issuecomment-1807181019
+        export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).${SWAY_PID}.sock
+        if [ -S "$SWAYSOCK" ]; then
+            break
+        fi
+    fi
+    sleep 0.5
+done
+
+# Check if socket is available before trying to reload
+if [ -n "$SWAYSOCK" ] && [ -S "$SWAYSOCK" ]; then
+    swaymsg reload
+else
+    echo "Sway socket not available, skipping reload" >&2
+    exit 0
+fi
 
 # Check whether to send notification
 if [ ! -f ~/.config/sway/config.json ]; then
